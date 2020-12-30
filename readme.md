49466365c67ad4eafc251788efeadac176a5d296
Kelley van Evert <hello@kelleyvanevert.nl>
2020-12-30 15:48:46 +0100

    setup

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Resources
+
+- http://tamivox.org/dave/boxchar/index.html
+- https://www.ludd.ltu.se/~vk/pics/ascii/junkyard/techstuff/tutorials/Hayley_Wakenshaw.html
+- https://jrgraphix.net/r/Unicode/2500-257F
---
+++ server.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const matter = require("gray-matter");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "content", "home"),
+    "utf8",
+    (err, filecontents) => {
+      if (err) {
+        res.status(404).send("Not found");
+      } else {
+        res.send(render(matter(filecontents)));
+      }
+    }
+  );
+});
+
+app.get("/recipe/:slug", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "content", "recipes", req.params.slug),
+    "utf8",
+    (err, filecontents) => {
+      if (err) {
+        res.status(404).send("Not found");
+      } else {
+        res.send(render(matter(filecontents)));
+      }
+    }
+  );
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log(`🎉 Server started, listening at http://localhost:${port}`);
+});
+
+function render({ data: { title, links = [] }, content }) {
+  for (const { text, to, color = "black" } of links) {
+    content = content.replace(
+      text,
+      `<a href="${to}" style="--color: ${color}">${text}</a>`
+    );
+  }
+
+  content = ("\n" + content.trim() + "\n")
+    .split("\n")
+    .map((line) => "  " + line + "  ")
+    .join("\n");
+
+  return `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${title}</title>
+    <style>
+    @font-face {
+      font-family: "DejaVu Sans Mono";
+      src: local("DejaVu Sans Mono"), url("/DejaVuSansMono.ttf") format("ttf");
+      font-weight: normal;
+    }
+    @font-face {
+      font-family: "DejaVu Sans Mono";
+      src: local("DejaVu Sans Mono Bold"), url("/DejaVuSansMono-Bold.ttf") format("ttf");
+      font-weight: bold;
+    }
+    html, body, pre {
+      margin: 0;
+      padding: 0;
+      font-family: "DejaVu Sans Mono";
+      font-size: 14px;
+      line-height: 128%;
+    }
+    a {
+      font-weight: bold;
+      text-decoration: none;
+      color: var(--color);
+      margin: -2px;
+      padding: 2px;
+    }
+    a:hover {
+      background: var(--color);
+      color: white;
+    }
+    </style>
+  </head>
+  <body>
+    <code><pre>${content}</pre></code>
+  </body>
+</html>
+`;
+}
