d4650c44bc62534f9556160533aad075a2dd3b27
Kelley van Evert <kelley@ijsfontein.nl>
2024-11-30 15:45:55 +0100

    ???

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,3 @@
-import { GetStaticProps } from "next";
-import { getData } from "../lib/recipes";
-import { MorphingLayout } from "../layouts/MorphingLayout";
+import { Entry } from "../layouts/Entry";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await getData();
-
-  const page = data.pages.find((p) => p.slug === "_404");
-
-  return {
-    props: {
-      page,
-      data,
-    },
-  };
-};
-
-export default MorphingLayout;
+export default Entry;
